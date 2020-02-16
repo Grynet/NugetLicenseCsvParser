c269c8c53a78cabb3731f4917d025e6210d72724
Grynet <nystad.fredrik@gmail.com>
2020-02-16 18:58:19 +0100

    Added LicenseCsvParser.cs & NuGet.cs

---
+++ LicenseCsvParser.cs
@@ -0,0 +1,76 @@
+﻿using NugetLicenseCsvParser.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace NugetLicenseCsvParser
+{
+    public class LicenseCsvParser
+    {
+        private ConcurrentDictionary<string, HashSet<NuGet>> _nugetsByLicenseUrl;
+
+        private readonly BufferBlock<string> _csvBuffer;
+        private readonly TransformBlock<string, NuGet> _parseBlock;
+        private readonly ActionBlock<NuGet> _aggregationBlock;       
+
+        public LicenseCsvParser()
+        {
+            _csvBuffer = new BufferBlock<string>();
+            _parseBlock = new TransformBlock<string, NuGet>(MapToNuget);
+            _aggregationBlock = new ActionBlock<NuGet>(AggregateNugets);
+
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+            _csvBuffer.LinkTo(_parseBlock, linkOptions);
+            _parseBlock.LinkTo(_aggregationBlock, linkOptions);
+        }
+
+        public ConcurrentDictionary<string, HashSet<NuGet>> ParseLicenseCsv(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentException($"{nameof(filePath)} cannot be null or empty");
+
+            _nugetsByLicenseUrl = new ConcurrentDictionary<string, HashSet<NuGet>>();
+
+            using (System.IO.StreamReader file = new System.IO.StreamReader(filePath))
+            {
+                string line = file.ReadLine();
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line.Replace("\"", "");
+                    _csvBuffer.Post(line);
+                }
+            }
+
+            _csvBuffer.Complete();
+            _aggregationBlock.Completion.Wait();
+            return _nugetsByLicenseUrl;
+        }
+
+        private NuGet MapToNuget(string csvLine)
+        {
+            var csv = csvLine.Split(',');
+            var nugetName = csv[0];
+            var nugetVersion = csv[1];
+            var nugetLicenseUrl = csv[2];
+
+            var nuget = new NuGet
+            {
+                Name = nugetName,
+                Version = nugetVersion,
+                LicenseUrl = string.IsNullOrWhiteSpace(nugetLicenseUrl) ? null : nugetLicenseUrl
+            };
+
+            return nuget;
+        }
+
+        private void AggregateNugets(NuGet nuget)
+        {
+            _nugetsByLicenseUrl.AddOrUpdate(nuget.LicenseUrl ?? "", new HashSet<NuGet> { nuget }, (licenseUrl, oldNugets) =>
+            {
+                oldNugets.Add(nuget);
+                return oldNugets;
+            });
+        }
+    }
+}
---
+++ Model/NuGet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NugetLicenseCsvParser.Model
+{
+    public class NuGet : IEquatable<NuGet>
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string LicenseUrl { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as NuGet);
+        }
+
+        public bool Equals(NuGet other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   Version == other.Version &&
+                   LicenseUrl == other.LicenseUrl;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Version, LicenseUrl);
+        }
+    }
+}
